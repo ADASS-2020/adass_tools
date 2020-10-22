2f8ba81abe0111f38550a50d16e0b778261899f5
Francesco Pierfederici <fpierfed@iram.es>
2020-10-22 12:27:40 +0200

    initial

---
+++ schedule_fixer_pipeline.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# Step 0: use the ADASS tools dir as wd and make sure that the Python env 
+# is in the PATH.
+cd /root/adass_tools
+export PATH=/root/env/bin:$PATH
+
+# Step 1: copy the original HTML export to a staging area
+/usr/bin/rsync -av --delete /var/pretalx/data/htmlexport/adass2020/ /tmp/adass2020/
+
+# Step 2: fix the index.html file with the info from the database (PIDs etc)
+/root/env/bin/python3 ./fix_titles_authors.py -e adass2020 /tmp/adass2020 > /tmp/index.html
+
+# Step 3: copy the modified index.html file to staging
+/bin/cp /tmp/index.html /tmp/adass2020/adass2020/schedule/index.html
+
+# Step 4: fix all calendar files to use UTC instead of CEST/CET/Madrid time
+./fix_calendars.sh -r /tmp/adass2020 -e adass2020
+
+# Step 5: Copy staging to prod
+/usr/bin/rsync -av --delete /tmp/adass2020/ /var/www/schedule/
