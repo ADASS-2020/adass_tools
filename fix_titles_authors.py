f38923f2c3bc5ffc379d40445e5e090e3e9d6992
Francesco Pierfederici <fpierfed@iram.es>
2020-10-22 18:18:45 +0200

    added option to edit talk and speaker lists to hide certain submission types (e.g. posters)

--- fix_titles_authors.py
+++ fix_titles_authors.py
@@ -19,6 +19,15 @@ TUTORIAL_IDS = (
     'WZ9YVR',
 )
 
+# Some times we do not want to display all the rooms. In these cases, we just
+# index the rooms in the schedule HTML page, starting from 0 as the left-most
+# room in the table/divs.
+ROOMS_TO_REMOVE = (1, )
+
+# Similarily, in Talks and Speakers we might want to hide some contribution
+# types (e.g. posters).
+TYPES_TO_REMOVE = (13, 18)
+
 CODES = {
  'P': 'Poster',
  'O': 'Oral Contribution',
@@ -50,6 +59,15 @@ ORDER BY
     submission_submission.id
 '''
 
+FIND_UNWANTED_TYPES = '''\
+SELECT
+    submission_submission.code
+FROM
+    submission_submission
+WHERE
+    submission_submission.submission_type_id in %s
+'''
+
 
 def mkcssclass(s):
     s = s.strip().replace(' ', '_')
@@ -65,9 +83,16 @@ def fix_auth_order(first_author, all_authors):
     )
 
 
+def _extract_code(a):
+    uri = a['href']
+    if uri.endswith('/'):
+        uri = uri[:-1]
+    code = uri.split('/')[-1]
+    return code
+
+
 def edit_index(event, changes, root):
-    schedule_dir = os.path.join(root, event, 'schedule')
-    index_path = os.path.join(schedule_dir, 'index.html')
+    index_path = os.path.join(root, event, 'schedule', 'index.html')
 
     with open(index_path) as f, open(ICS_SVG) as isvg, open(XML_SVG) as xsvg:
         soup = BeautifulSoup(f, 'html.parser')
@@ -81,6 +106,13 @@ def edit_index(event, changes, root):
             tag['width'] = containers[i].svg['width']
             tag['height'] = containers[i].svg['height']
             containers[i].svg.replaceWith(tag)
+
+        # Hide rooms, if needed.
+        for class_name in ('pretalx-schedule-day-room-header',
+                           'pretalx-schedule-room'):
+            for i, div in enumerate(soup.find_all('div', class_name)):
+                if i in ROOMS_TO_REMOVE:
+                    div['style'] = 'display: none;'
 
         # Fix titles and authors
         for container in soup.find_all('div', 'pretalx-schedule-talk'):
@@ -105,12 +137,83 @@ def edit_index(event, changes, root):
             title_span.string = new_title
             authors_span.string = fix_auth_order(first_author, all_authors)
             container['class'].append(mkcssclass(new_title.split(' (')[0]))
-    print(soup.prettify())
+    return soup.prettify()
+
+
+def edit_talk(event, subs_to_hide, root):
+    talk_path = os.path.join(root, event, 'talk', 'index.html')
+
+    with open(talk_path) as f:
+        soup = BeautifulSoup(f, 'html.parser')
+        for section in soup.find_all('section'):
+            h3 = section.find('h3', 'talk-title')
+            if not h3:
+                continue
+
+            code = _extract_code(h3.a)
+            if code not in subs_to_hide:
+                continue
+
+            section['style'] = 'display: none;'
+    return soup.prettify()
+
+
+def edit_speaker(event, subs_to_hide, root):
+    talk_path = os.path.join(root, event, 'speaker', 'index.html')
+
+    with open(talk_path) as f:
+        soup = BeautifulSoup(f, 'html.parser')
+        for section in soup.find_all('section'):
+            h3 = section.find('h3', 'talk-title')
+            if not h3:
+                continue
+
+            p = section.p
+            if not p:
+                continue
+
+            links = p.find_all('a')
+            codes = set(_extract_code(tag) for tag in links)
+            bad = codes.intersection(subs_to_hide)
+
+            # Here we have three cases:
+            # 1. author has no contribution to hide: leave as is
+            if not bad:
+                continue
+
+            # 2. author has only contributions we want to hide
+            if bad == codes:
+                # Hide the author and move on
+                section['style'] = 'display: none;'
+                continue
+
+            # 3. author has a mixture of contributions, some good, some bad
+            new_content = []
+            for el in p.contents:
+                if el.name == 'a' and _extract_code(el) in bad:
+                    # Remove the latest thing we added to new_content if any
+                    # because that would be a "|"
+                    if new_content:
+                        new_content.pop()
+                    # and skip ahead
+                    continue
+                else:
+                    new_content.append(el)
+            # Now replace the old content with the new one
+            p.contents = new_content
+    return soup.prettify()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--event', '-e', type=str, help='event name')
+    parser.add_argument('--event', '-e', type=str, required=True,
+                        help='event name')
+    parser.add_argument('--index', '-i', type=str, required=True,
+                        help='new index file')
+    parser.add_argument('--talk', '-t', type=str, required=True,
+                        help='new talk file')
+    parser.add_argument('--speaker', '-s', type=str, required=True,
+                        help='new speaker file')
     parser.add_argument('root', metavar='HTML_ROOT', type=str, nargs=1,
                         help='root of the pretalx HTML export')
     args = parser.parse_args()
@@ -142,4 +245,19 @@ if __name__ == '__main__':
 
         changes[code] = (new_title, first_author)
 
-    edit_index(args.event, changes, root)
+    # Submissions to hide:
+    subs_to_hide = []
+    if TYPES_TO_REMOVE:
+        cur.execute(FIND_UNWANTED_TYPES % (str(tuple(TYPES_TO_REMOVE))))
+        subs_to_hide = set(res[0] for res in cur.fetchall())
+
+    index = edit_index(args.event, changes, root)
+    talk = edit_talk(args.event, subs_to_hide, root)
+    speaker = edit_speaker(args.event, subs_to_hide, root)
+
+    with open(args.index, 'w') as f:
+        f.write(index)
+    with open(args.talk, 'w') as f:
+        f.write(talk)
+    with open(args.speaker, 'w') as f:
+        f.write(speaker)
--- schedule_fixer_pipeline.sh
+++ schedule_fixer_pipeline.sh
@@ -10,10 +10,12 @@ export PATH=/root/env/bin:$PATH
 /usr/bin/rsync -av --delete /var/pretalx/data/htmlexport/adass2020/ /tmp/adass2020/
 
 # Step 2: fix the index.html file with the info from the database (PIDs etc)
-/root/env/bin/python3 ./fix_titles_authors.py -e adass2020 /tmp/adass2020 > /tmp/index.html
+/root/env/bin/python3 ./fix_titles_authors.py -e adass2020 /tmp/adass2020 -i /tmp/index.html -t /tmp/talk.html -s /tmp/speaker.html
 
 # Step 3: copy the modified index.html file to staging
-/bin/cp /tmp/index.html /tmp/adass2020/adass2020/schedule/index.html
+/bin/mv /tmp/index.html /tmp/adass2020/adass2020/schedule/index.html
+/bin/mv /tmp/talk.html /tmp/adass2020/adass2020/talk/index.html
+/bin/mv /tmp/speaker.html /tmp/adass2020/adass2020/speaker/index.html
 
 # Step 4: fix all calendar files to use UTC instead of CEST/CET/Madrid time
 ./fix_calendars.sh -r /tmp/adass2020 -e adass2020
