aba6e670b6849e5cffd2a96bdeee8a1a4f9becb6
Francesco Pierfederici <fpierfed@iram.es>
2020-10-22 12:27:27 +0200

    small changes to the sed cmd

--- fix_calendars.sh
+++ fix_calendars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 usage() {
     echo "usage: fix_calendars.sh -r|--root HTML_EXPORT_DIR -e|--event EVENT_NAME"
@@ -28,10 +29,10 @@ while [ "$1" != "" ]; do
     shift
 done
 
-find $root -type f -name \*.xml -exec sed -i .orig 's/\+01:00/\+00:00/g' {} \; -print
-find $root -type f -name schedule.json -exec sed -i .orig 's/\+01:00/\+00:00/g' {} \; -print
-find $root -type f -name \*.ics -exec sed -i .orig -E 's/;TZID=Europe\/Madrid:([0-9T]+)/:\1Z/g' {} \; -print
-find $root -type f -name \*.ics -exec sed -i .orig 's/https:\/\/pretalx\./https:\/\/schedule\./g' {} \; -print
+find $root -type f -name \*.xml -exec sed -i.orig 's/\+01:00/\+00:00/g' {} \; -print
+find $root -type f -name schedule.json -exec sed -i.orig 's/\+01:00/\+00:00/g' {} \; -print
+find $root -type f -name \*.ics -exec sed -i.orig -E 's/;TZID=Europe\/Madrid:([0-9T]+)/:\1Z/g' {} \; -print
+find $root -type f -name \*.ics -exec sed -i.orig 's/https:\/\/pretalx\./https:\/\/schedule\./g' {} \; -print
 
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 schedule_convert $root/$event/schedule/export/schedule.xml -l $tmp_dir https://schedule.adass2020.es
